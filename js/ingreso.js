e6ebc85bb70ef0cdc787c0eed1529807ba3294b5
FlorFarruggia <flor.farru@gmail.com>
2023-08-16 21:06:02 -0300

    Se agregó el modal de recovery password al ingreso y se agreg{o el elemento user-select a todos los Main.

--- js/ingreso.js
+++ js/ingreso.js
@@ -2,6 +2,14 @@ const openModal = document.querySelector('.buttonLog');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modalClose');
 
+const forgotPswd = document.querySelector('.forgotPswd');
+const modalPass = document.querySelector('.modalPass');
+const enviarButton = document.querySelector('.enviar');
+const tittlePass = document.querySelector('.tittle');
+const inputEmail = document.getElementById('emailPass');
+const checkTittle = document.querySelector('.check');
+const closePassModal = document.querySelector('.modalPassClose');
+
 openModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.add('modalShow');
@@ -10,4 +18,22 @@ openModal.addEventListener('click', (e)=>{
 closeModal.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.classList.remove('modalShow');
+});
+
+forgotPswd.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modalPass.classList.add('modalPassShow');
+    tittlePass.style.display = 'block';
+    inputEmail.style.display = 'block';
+    checkTittle.style.display = 'none';
+});
+enviarButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+    tittlePass.style.display = 'none';
+    inputEmail.style.display = 'none';
+    checkTittle.style.display = 'block';
+});
+closePassModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modalPass.classList.remove('modalPassShow');
 });
