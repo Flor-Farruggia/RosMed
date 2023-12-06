f261fc34597f3c96cbde74aec41d2f5bab3707cf
FlorFarruggia <[flor.farru@gmail.com]>
2023-12-06 00:38:28 -0300

    arreglo menu

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-const menuIconoMin = document.querySelector('.icono-menu');
-const menuDrop = document.querySelector('.menu-nav-min');
-
-
-menuIconoMin.addEventListener('click', (e)=>{
-    e.preventDefault();
-    menuDrop.classList.add('menu-nav-min-show');
-});
